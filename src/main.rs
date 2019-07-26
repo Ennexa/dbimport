b04e43a25269e26bec4fb8539d95510cff244f83
Joyce Babu <joyce@ennexa.com>
2019-07-26 13:56:20 +0530

    feat: load default values from config file

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,86 @@ use std::fs::File;
 use tempfile::Builder;
 use bzip2::read::BzDecoder;
 use std::process::{Command, Stdio};
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use std::error::Error;
 
-fn main() {
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct Config {
+	ssh_host: String,
+	#[serde(default = "Config::default_port")]
+	ssh_port: String,
+	ssh_user: String,
+	db_user: Option<String>,
+	db_pass: Option<String>,
+	db_name: Option<String>,
+}
+
+impl Config {
+	pub fn default_port() -> String {
+		return "22".to_string()
+	}
+	pub fn load(host: &str) -> Config {
+	    let config_file = dirs::home_dir().unwrap().join(".config/dbimport.yml");
+
+	    let re = Regex::new(r"^(?:([a-z0-9-]+)@)?([a-z0-9_.-]+)(?::(\d+))?$").unwrap();
+	    let cap = re.captures(host).unwrap();
+
+		let ssh_host = cap.get(2).unwrap_or_else(|| {
+			eprintln!("Failed to parse ssh host");
+			process::exit(1);
+		}).as_str();
+
+	    let mut config:Config = match config_file.exists() {
+	    	// let config_path = ;
+	    	true => Config::parse(config_file.to_str().unwrap(), ssh_host),
+	    	false => Config {
+	    		ssh_host: ssh_host.to_string(),
+	    		ssh_port: "22".to_string(),
+	    		..Default::default()
+	    	},
+	    };
+
+	    match cap.get(1) {
+	    	Some(val) => config.ssh_user = val.as_str().to_string(),
+	    	None => {
+	    		eprintln!("SSH username cannot be empty");
+	    		process::exit(1);
+	    	}
+		}
+
+        if let Some(ssh_port) = cap.get(3) {
+        	config.ssh_port = ssh_port.as_str().to_string();
+        }
+
+        config
+	}
+
+	pub fn parse(path: &str, host: &str) -> Config {
+
+		match Config::parse_config_file(path, host) {
+			Ok(config) => config,
+			Err(err) => {
+				eprintln!("Failed to parse config file - {}", err);
+				Config {
+					ssh_host: host.to_string(),
+					..Default::default()
+				}
+			},
+		}
+	}
+
+	fn parse_config_file(path: &str, host: &str) -> Result<Config, Box<Error>> {
+		let f = std::fs::File::open(path)?;
+		let mut config: HashMap<String, Config> = serde_yaml::from_reader(f)?;
+		match config.remove(host) {
+			Some(n) => Ok(n),
+			None => Err(From::from("No matching cities with a population were found.")),
+		}
+	}
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
     let matches = App::new("Database Importer")
         .about("Import database from remote server")
         .arg(Arg::with_name("database")
@@ -69,6 +147,9 @@ fn main() {
             process::exit(1);
         }
     };
+
+    let config = Config::load(host);
+
     let tables = match matches.values_of("table")
     {
         Some(n) => n,
@@ -81,47 +162,48 @@ fn main() {
     let extra_args = matches.value_of("extra_args").unwrap_or("");
     let out_file = matches.value_of("out_file");
 
-    let re = Regex::new(r"^(?:([a-z0-9-]+)@)?([a-z0-9_.-]+)(?::(\d+))?$").unwrap();
-    let cap = re.captures(host).unwrap();
-
-    let ssh_user = match cap.get(1) {
-        Some(v) => v.as_str(),
-        None => {
-            eprintln!("Failed to detect database username");
-            process::exit(1);
-        },
-    };
-    let ssh_host = cap.get(2).unwrap().as_str();
-    let ssh_port = match cap.get(3) {
-        Some(v) => v.as_str(),
-        None => "22"
-    };
-
     // Try to fetch username from command-line options
     let db_user = match matches.value_of("dbuser") {
-        Some(v) => v,
-        None => {
-            // User option was not specified.
-            // Use ssh username, if available
-            eprintln!("Using SSH username as database username");
-            ssh_user
-        }
+    	Some(val) => val.to_owned(),
+    	None => match config.db_user {
+    		Some(val) => val,
+    		None => {
+	            // User option was not specified.
+	            // Use ssh username, if available
+	            eprintln!("Using SSH username as database username");
+    			config.ssh_user.to_owned()
+    		}
+    	}
     };
 
     let db_name = match matches.value_of("database") {
         Some(v) => v.to_string(),
-        None => {
-            // User option was not specified.
-            // Use ssh username, if available
-            eprintln!("Inferring database name as {}_db", db_user);
-            format!("{}_db", db_user)
+        None => match config.db_name {
+        	Some(v) => v,
+        	None => {
+	            // User option was not specified.
+	            // Use ssh username, if available
+	            eprintln!("Inferring database name as {}_db", db_user);
+	            format!("{}_db", db_user)
+        	}
         }
     };
 
-    let db_pass = matches.value_of("dbpass").unwrap_or("");
+    let db_pass = match matches.value_of("db_pass") {
+    	Some(val) => val.to_owned(),
+    	None => match config.db_pass {
+    		Some(val) => val,
+    		None => {
+    			eprintln!("Database password cannot be empty");
+    			process::exit(2);
+    		}
+    	}
+    };
+
+    //let db_pass = matches.value_of("dbpass").unwrap_or("");
 
     // Connect to the remote server
-    let ssh_host_port = format!("{}:{}", ssh_host, ssh_port);
+    let ssh_host_port = format!("{}:{}", config.ssh_host, config.ssh_port);
     let tcp = TcpStream::connect(&ssh_host_port).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
@@ -132,7 +214,7 @@ fn main() {
 
     // Try to authenticate with the first identity in the agent.
     eprint!("Attept to authenticate with ssh-agent...");
-    let _ = sess.userauth_agent(ssh_user);
+    let _ = sess.userauth_agent(&config.ssh_user);
 
     // Make sure we succeeded
     if !sess.authenticated() {
@@ -142,7 +224,7 @@ fn main() {
         eprint!("Enter password: ");
 
         let ssh_pass = rpassword::read_password().unwrap();
-        sess.userauth_password(&ssh_user, &ssh_pass).unwrap_or_else(|_| {
+        sess.userauth_password(&config.ssh_user, &ssh_pass).unwrap_or_else(|_| {
             eprintln!("Failed to authenticate to remote server");
             process::exit(1);
         });
@@ -163,10 +245,10 @@ fn main() {
     //     });
     // }).expect("Error setting Ctrl-C handler");
 
-    let pass_arg = format!("-p{}", db_pass);
+    let pass_arg = format!("-p{}", &db_pass);
     let mut v = vec![
         "mysqldump",
-        "-u", db_user,
+        "-u", &db_user,
         &pass_arg,
         &db_name,
     ];
@@ -235,7 +317,7 @@ fn main() {
     let f = BufReader::new(File::open(&path).unwrap());
 
     let v = vec![
-        "-u", db_user,
+        "-u", &db_user,
         &pass_arg,
         &db_name
     ];
@@ -274,5 +356,5 @@ fn main() {
     	n => eprintln!("Import failed with exit code {:?}", n),
     };
 
-    ()
+    Ok(())
 }
