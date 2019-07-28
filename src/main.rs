4cfe8631899db6879a49aeed431be547eacc6017
Joyce Babu <joyce@ennexa.com>
2019-07-29 02:48:14 +0530

    style: replace tab character with spaces

--- src/main.rs
+++ src/main.rs
@@ -22,77 +22,77 @@ use std::error::Error;
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 struct Config {
-	ssh_host: String,
-	#[serde(default = "Config::default_port")]
-	ssh_port: String,
-	ssh_user: String,
-	db_user: Option<String>,
-	db_pass: Option<String>,
-	db_name: Option<String>,
+    ssh_host: String,
+    #[serde(default = "Config::default_port")]
+    ssh_port: String,
+    ssh_user: String,
+    db_user: Option<String>,
+    db_pass: Option<String>,
+    db_name: Option<String>,
 }
 
 impl Config {
-	pub fn default_port() -> String {
-		return "22".to_string()
-	}
-	pub fn load(host: &str) -> Config {
-	    let config_file = dirs::home_dir().unwrap().join(".config/dbimport.yml");
+    pub fn default_port() -> String {
+        return "22".to_string()
+    }
+    pub fn load(host: &str) -> Config {
+        let config_file = dirs::home_dir().unwrap().join(".config/dbimport.yml");
 
-	    let re = Regex::new(r"^(?:([a-z0-9-]+)@)?([a-z0-9_.-]+)(?::(\d+))?$").unwrap();
-	    let cap = re.captures(host).unwrap();
+        let re = Regex::new(r"^(?:([a-z0-9-]+)@)?([a-z0-9_.-]+)(?::(\d+))?$").unwrap();
+        let cap = re.captures(host).unwrap();
 
-		let ssh_host = cap.get(2).unwrap_or_else(|| {
-			eprintln!("Failed to parse ssh host");
-			process::exit(1);
-		}).as_str();
+        let ssh_host = cap.get(2).unwrap_or_else(|| {
+            eprintln!("Failed to parse ssh host");
+            process::exit(1);
+        }).as_str();
 
-	    let mut config:Config = match config_file.exists() {
-	    	// let config_path = ;
-	    	true => Config::parse(config_file.to_str().unwrap(), ssh_host),
-	    	false => Config {
-	    		ssh_host: ssh_host.to_string(),
-	    		ssh_port: "22".to_string(),
-	    		..Default::default()
-	    	},
-	    };
+        let mut config:Config = match config_file.exists() {
+            // let config_path = ;
+            true => Config::parse(config_file.to_str().unwrap(), ssh_host),
+            false => Config {
+                ssh_host: ssh_host.to_string(),
+                ssh_port: "22".to_string(),
+                ..Default::default()
+            },
+        };
 
-	    match cap.get(1) {
-	    	Some(val) => config.ssh_user = val.as_str().to_string(),
-	    	None => {
-	    		eprintln!("SSH username cannot be empty");
-	    		process::exit(1);
-	    	}
-		}
+        match cap.get(1) {
+            Some(val) => config.ssh_user = val.as_str().to_string(),
+            None => {
+                eprintln!("SSH username cannot be empty");
+                process::exit(1);
+            }
+        }
 
         if let Some(ssh_port) = cap.get(3) {
-        	config.ssh_port = ssh_port.as_str().to_string();
+            config.ssh_port = ssh_port.as_str().to_string();
         }
 
         config
-	}
+    }
 
-	pub fn parse(path: &str, host: &str) -> Config {
+    pub fn parse(path: &str, host: &str) -> Config {
 
-		match Config::parse_config_file(path, host) {
-			Ok(config) => config,
-			Err(err) => {
-				eprintln!("Failed to parse config file - {}", err);
-				Config {
-					ssh_host: host.to_string(),
-					..Default::default()
-				}
-			},
-		}
-	}
+        match Config::parse_config_file(path, host) {
+            Ok(config) => config,
+            Err(err) => {
+                eprintln!("Failed to parse config file - {}", err);
+                Config {
+                    ssh_host: host.to_string(),
+                    ..Default::default()
+                }
+            },
+        }
+    }
 
-	fn parse_config_file(path: &str, host: &str) -> Result<Config, Box<Error>> {
-		let f = std::fs::File::open(path)?;
-		let mut config: HashMap<String, Config> = serde_yaml::from_reader(f)?;
-		match config.remove(host) {
-			Some(n) => Ok(n),
-			None => Err(From::from("No matching cities with a population were found.")),
-		}
-	}
+    fn parse_config_file(path: &str, host: &str) -> Result<Config, Box<Error>> {
+        let f = std::fs::File::open(path)?;
+        let mut config: HashMap<String, Config> = serde_yaml::from_reader(f)?;
+        match config.remove(host) {
+            Some(n) => Ok(n),
+            None => Err(From::from("No matching cities with a population were found.")),
+        }
+    }
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
@@ -164,40 +164,40 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     // Try to fetch username from command-line options
     let db_user = match matches.value_of("dbuser") {
-    	Some(val) => val.to_owned(),
-    	None => match config.db_user {
-    		Some(val) => val,
-    		None => {
-	            // User option was not specified.
-	            // Use ssh username, if available
-	            eprintln!("Using SSH username as database username");
-    			config.ssh_user.to_owned()
-    		}
-    	}
+        Some(val) => val.to_owned(),
+        None => match config.db_user {
+            Some(val) => val,
+            None => {
+                // User option was not specified.
+                // Use ssh username, if available
+                eprintln!("Using SSH username as database username");
+                config.ssh_user.to_owned()
+            }
+        }
     };
 
     let db_name = match matches.value_of("database") {
         Some(v) => v.to_string(),
         None => match config.db_name {
-        	Some(v) => v,
-        	None => {
-	            // User option was not specified.
-	            // Use ssh username, if available
-	            eprintln!("Inferring database name as {}_db", db_user);
-	            format!("{}_db", db_user)
-        	}
+            Some(v) => v,
+            None => {
+                // User option was not specified.
+                // Use ssh username, if available
+                eprintln!("Inferring database name as {}_db", db_user);
+                format!("{}_db", db_user)
+            }
         }
     };
 
     let db_pass = match matches.value_of("db_pass") {
-    	Some(val) => val.to_owned(),
-    	None => match config.db_pass {
-    		Some(val) => val,
-    		None => {
-    			eprintln!("Database password cannot be empty");
-    			process::exit(2);
-    		}
-    	}
+        Some(val) => val.to_owned(),
+        None => match config.db_pass {
+            Some(val) => val,
+            None => {
+                eprintln!("Database password cannot be empty");
+                process::exit(2);
+            }
+        }
     };
 
     //let db_pass = matches.value_of("dbpass").unwrap_or("");
@@ -241,7 +241,7 @@ fn main() -> Result<(), Box<std::error::Error>> {
     // ctrlc::set_handler(move || {
     //     // Handle early termination
     //     let status = sess.sftp().and_then(|sftp| {
-    //     	sftp.unlink(Path::new(remote_temp_file))
+    //         sftp.unlink(Path::new(remote_temp_file))
     //     });
     // }).expect("Error setting Ctrl-C handler");
 
@@ -257,7 +257,9 @@ fn main() -> Result<(), Box<std::error::Error>> {
         v.push(table);
     }
 
-    let v:Vec<String> = v.into_iter().map(|item| shell_escape::unix::escape(std::borrow::Cow::from(item)).into_owned()).collect();
+    let v:Vec<String> = v.into_iter().map(|item| {
+        shell_escape::unix::escape(std::borrow::Cow::from(item)).into_owned()
+    }).collect();
     let arg = format!("{} {} | bzip2 > {}", v.join(" "), extra_args, remote_temp_file);
 
     eprint!("Exporting database on target server...");
@@ -270,7 +272,7 @@ fn main() -> Result<(), Box<std::error::Error>> {
     match exit_status {
         Ok(0) => (),
         _ => {
-        	eprintln!("ERR");
+            eprintln!("ERR");
             eprintln!("Failed to export database");
             process::exit(4);
         }
@@ -291,8 +293,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
         .expect("Failed to create temporary file");
 
     let path = match out_file {
-    	Some(n) => Path::new(n),
-    	None => temp_file.path(),
+        Some(n) => Path::new(n),
+        None => temp_file.path(),
     };
 
     let mut target = File::create(path).unwrap();
@@ -338,32 +340,32 @@ fn main() -> Result<(), Box<std::error::Error>> {
          .expect("Failed to spawn mysql client");
 
     if let Some(stdin) = &mut cmd.stdin {
-	    let mut writer = BufWriter::new(stdin);
+        let mut writer = BufWriter::new(stdin);
 
-	    // Decompress and import to local mysql database
-	    match copy(&mut Box::new(BzDecoder::new(f)), &mut writer) {
-	        Ok(_) => {
-	        	eprintln!("Import completed successfully")
-	        },
-	        Err(err) => {
-	            eprintln!("Failed to import database dump - {}", err);
-	            process::exit(5);
-	        }
-	    };
+        // Decompress and import to local mysql database
+        match copy(&mut Box::new(BzDecoder::new(f)), &mut writer) {
+            Ok(_) => {
+                eprintln!("Import completed successfully")
+            },
+            Err(err) => {
+                eprintln!("Failed to import database dump - {}", err);
+                process::exit(5);
+            }
+        };
     }
 
     let result = match cmd.try_wait() {
-    	Ok(Some(status)) => status.code(),
-	    Ok(None) => cmd.wait().unwrap().code(),
-	    Err(e) => {
-	    	eprintln!("error attempting to wait: {}", e);
-	    	process::exit(5);
-	    }
+        Ok(Some(status)) => status.code(),
+        Ok(None) => cmd.wait().unwrap().code(),
+        Err(e) => {
+            eprintln!("error attempting to wait: {}", e);
+            process::exit(5);
+        }
     };
 
     match result {
-    	Some(0) => (),
-    	n => eprintln!("Import failed with exit code {:?}", n),
+        Some(0) => (),
+        n => eprintln!("Import failed with exit code {:?}", n),
     };
 
     Ok(())
